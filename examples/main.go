11bcb26858d15ba0b82efce24e9f13ff8be75381
treilik <treilik@posteo.de>
2022-12-14 18:51:48 +0100

    add spinner to example

--- examples/main.go
+++ examples/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	boxer "github.com/treilik/bubbleboxer"
 )
 
 const (
+	upperAddr  = "upper"
 	leftAddr   = "left"
 	middleAddr = "middle"
 	rightAddr  = "right"
@@ -16,6 +18,7 @@ const (
 
 func main() {
 	// leaf content creation (models)
+	upper := spinnerHolder{spinner.NewModel()}
 	left := stringer(leftAddr)
 	middle := stringer(middleAddr)
 	right := stringer(rightAddr)
@@ -31,13 +34,15 @@ func main() {
 		SizeFunc: func(_ boxer.Node, widthOrHeight int) []int {
 			return []int{
 				// since this node is vertical stacked return the height partioning since the width stays for all children fixed
-				widthOrHeight - 1,
+				1,
+				widthOrHeight - 2,
 				1,
 				// make also sure that the amount of the returned ints match the amount of children:
 				// in this case two, but in more complex cases read the amount of the chilren from the len(boxer.Node.Children)
 			}
 		},
 		Children: []boxer.Node{
+			m.tui.CreateLeaf(upperAddr, upper),
 			{
 				Children: []boxer.Node{
 					// make sure to encapsulate the models into a leaf with CreateLeaf:
@@ -61,7 +66,9 @@ type model struct {
 	tui boxer.Boxer
 }
 
-func (m model) Init() tea.Cmd { return nil }
+func (m model) Init() tea.Cmd {
+	return spinner.Tick
+}
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
@@ -71,11 +78,34 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	case tea.WindowSizeMsg:
 		m.tui.UpdateSize(msg)
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.editModel(upperAddr, func(v tea.Model) (tea.Model, error) {
+			v, cmd = v.Update(msg)
+			return v, nil
+		})
+		return m, cmd
 	}
 	return m, nil
 }
 func (m model) View() string {
 	return m.tui.View()
+}
+
+func (m *model) editModel(addr string, edit func(tea.Model) (tea.Model, error)) error {
+	if edit == nil {
+		return fmt.Errorf("no edit function provided")
+	}
+	v, ok := m.tui.ModelMap[addr]
+	if !ok {
+		fmt.Errorf("no model with address '%s' found", addr)
+	}
+	v, err := edit(v)
+	if err != nil {
+		return err
+	}
+	m.tui.ModelMap[addr] = v
+	return nil
 }
 
 type stringer string
@@ -88,3 +118,19 @@ func (s stringer) String() string {
 func (s stringer) Init() tea.Cmd                           { return nil }
 func (s stringer) Update(msg tea.Msg) (tea.Model, tea.Cmd) { return s, nil }
 func (s stringer) View() string                            { return s.String() }
+
+type spinnerHolder struct {
+	m spinner.Model
+}
+
+func (s spinnerHolder) Init() tea.Cmd {
+	return s.m.Tick
+}
+func (s spinnerHolder) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	m, cmd := s.m.Update(msg)
+	s.m = m
+	return s, cmd
+}
+func (s spinnerHolder) View() string {
+	return s.m.View()
+}
--- go.mod
+++ go.mod
@@ -3,19 +3,21 @@ module github.com/treilik/bubbleboxer
 go 1.17
 
 require (
-	github.com/charmbracelet/bubbletea v0.19.3
+	github.com/charmbracelet/bubbles v0.14.0
+	github.com/charmbracelet/bubbletea v0.21.0
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b
 )
 
 require (
-	github.com/containerd/console v1.0.2 // indirect
+	github.com/charmbracelet/lipgloss v0.5.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/cancelreader v0.2.0 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
-	github.com/muesli/termenv v0.9.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
-	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
